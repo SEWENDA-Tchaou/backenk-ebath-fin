e335ee09b49e04688afac57e9919e75cb714572a
sewenda tchaou <sewendatchaou@gmail.com>
2023-11-03 14:58:27 -0700

    remove something again

--- server.js
+++ server.js
@@ -168,7 +168,10 @@ app.post("/commentForUs", (req, res) =>{
         if(err){
             res.json("error")
         }else{
-            res.json(data)
+            res.res.set({
+                "Access-Control-Allow-Origin":"https://ebath-site.vercel.app",
+                // "Access-Control-Allow-Credentials": true
+            }).json(data)
         }
     })
 })
